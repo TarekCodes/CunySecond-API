2d3dbc5354df0e41b8d59dbb6aba664808758af6
Tarek <tareksaidee@hotmail.com>
2018-04-17 21:21:24 -0400

    add springboot setup

---
+++ src/main/java/com/tareksaidee/cunysecond/CunySecondApplication.java
@@ -0,0 +1,12 @@
+package com.tareksaidee.cunysecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunySecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CunySecondApplication.class, args);
+    }
+}
