fe3f653084ac6d64aa5f6d314aa9328fbc9aaa80
Tarek <tareksaidee@hotmail.com>
2018-04-18 23:46:20 -0400

    add firebase4j setup

--- src/main/java/com/tareksaidee/cunysecond/controller/DataController.java
+++ src/main/java/com/tareksaidee/cunysecond/controller/DataController.java
@@ -1,14 +1,20 @@
 package com.tareksaidee.cunysecond.controller;
 
+import net.thegreshams.firebase4j.error.FirebaseException;
+import net.thegreshams.firebase4j.service.Firebase;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class DataController {
 
+    private static String firebase_baseUrl = "https://cunysecond.firebaseio.com/";
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String index() throws FirebaseException, Exception {
+        Firebase firebase = new Firebase( firebase_baseUrl );
+        return firebase.get(firebase_baseUrl.concat("brooklyn_college")).getRawBody();
     }
 }
